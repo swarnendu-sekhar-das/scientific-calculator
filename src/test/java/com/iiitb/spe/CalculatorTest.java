662f6733f69a015dca489e4f095c2603379bf773
Swarnendu Sekhar Das <66134817+swarnendu-sekhar-das@users.noreply.github.com>
2026-02-28 19:33:32 +0530

    Added test cases for all operations

--- src/test/java/com/iiitb/spe/CalculatorTest.java
+++ src/test/java/com/iiitb/spe/CalculatorTest.java
@@ -6,8 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
 
     @Test
+    void testAddition() {
+        assertEquals(10.0, Calculator.add(6, 4), 0.0001);
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(2.0, Calculator.subtract(6, 4), 0.0001);
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(24.0, Calculator.multiply(6, 4), 0.0001);
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2.0, Calculator.divide(8, 4), 0.0001);
+    }
+
+    @Test
     void testSquareRoot() {
-        assertEquals(4.0, Calculator.squareRoot(16));
+        assertEquals(4.0, Calculator.squareRoot(16), 0.0001);
     }
 
     @Test
@@ -17,11 +37,11 @@ public class CalculatorTest {
 
     @Test
     void testNaturalLog() {
-        assertEquals(Math.log(10), Calculator.naturalLog(10));
+        assertEquals(Math.log(10), Calculator.naturalLog(10), 0.0001);
     }
 
     @Test
     void testPower() {
-        assertEquals(8, Calculator.power(2,3));
+        assertEquals(8.0, Calculator.power(2, 3), 0.0001);
     }
 }
